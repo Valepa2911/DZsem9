b70ee353a86a55d0ecf4750bf57dbbb9ebd6d213
Valepa2911 <megp17102017@mail.ru>
2022-12-17 22:53:26 +0400

    сохран.

--- Zadacha66/Program.cs
+++ Zadacha66/Program.cs
@@ -4,16 +4,15 @@
 
 Console.WriteLine("Ведите число: m");
 int m = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Ведите число: n");
+Console.WriteLine("Ведите число: n (больше m)");
 int n = Convert.ToInt32(Console.ReadLine());
 
 int SumNatural(int m, int n)
 {
-    if (m > n) return 0; 
-    {
-         return n + SumNatural(m, n - 1);
-    }
-   
+    if (m == 0) return (n * (n + 1)) / 2;
+    if (n == 0) return m;
+    if (m >= n) return m;
+    return n + SumNatural(m, n - 1);
 }
 
 Console.WriteLine(SumNatural(m, n));
